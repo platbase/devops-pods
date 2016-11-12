60964ed1a6bcfdfdfbffa100611e5a73b7f833fa
thinkbase <thinkbase@users.noreply.github.com>
2016-11-12 09:33:00 +0800

    Merge nodejs and nodejs-debug, make start command colorful.

--- develop-tools/nodejs/docker-node.sh
+++ develop-tools/nodejs/docker-node.sh
@@ -3,9 +3,39 @@
 # SHELL_ROOT - Location(Path) of this batch file
 SHELL_ROOT=$(cd "$(dirname "$0")"; pwd)
 
-mkdir -p "${SHELL_ROOT}/.runtime/cache"
-mkdir -p "${SHELL_ROOT}/.runtime/global"
-touch "${SHELL_ROOT}/.runtime/devops.platbase.com-docker-data"
+# Text color variables
+txtund=$(tput sgr 0 1)          # Underline
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+
+# The volumes location
+if [ -z $VOL_DATA ]; then
+    VOL_DATA="${SHELL_ROOT}/.runtime"
+fi
+if [ -z $VOL_WS ]; then
+    VOL_WS="`pwd`"
+fi
+
+echo ">>>${bldgrn} /data ${txtrst} should be mounted to ${bldgrn} $VOL_DATA ${txtrst}."
+echo ">>>${bldgrn} /workspace  ${txtrst} should be mounted to ${bldgrn} $VOL_WS ${txtrst}."
+read -p "${bldred} Continue to start NodeJS?${txtrst} [Y|n]: " CONFIRM_CHAR
+case $CONFIRM_CHAR in 
+Y|y)
+    ;;
+*)
+    echo ">>> ${bldwht}User abort to start NodeJS: [$CONFIRM_CHAR] ${txtrst}."
+    exit -100
+    ;;
+esac
+
+# Prepare /data volume
+mkdir -p "$VOL_DATA/cache"
+mkdir -p "$VOL_DATA/global"
+touch "$VOL_DATA/devops.platbase.com-docker-data"
 
 # Find avaliable ports between 8080 to 9080
 LOWERPORT=8080
@@ -19,6 +49,6 @@ for (( port = LOWERPORT ; port <= UPPERPORT ; port++ )); do
 done
 [ "$lp" = "null" ] && { echo "No free local ports available(between $LOWERPORT and $UPPERPORT)"; exit 2; }
 
-echo ">>> Find avaliable local port: $port ."
+echo ">>> Find avaliable local port: ${bldgrn} $port ${txtrst}."
 
-docker run -it --rm -p $port:8080 -v "`pwd`":/workspace -v "${SHELL_ROOT}/.runtime":/data platbase.com/dev.nodejs:6.2.1
+docker run -it --rm -p $port:8080 -v "$VOL_WS":/workspace -v "$VOL_DATA":/data platbase.com/dev.nodejs:6.2.1
