59e5ccec81ae992f32ab7408566d8082bf5a41b0
thinkbase <thinkbase.net@gmail.com>
2021-07-02 23:38:50 +0800

    feature: reconstruct folders

--- develop-tools/mongodb/files/start-mongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "*** Development environment - mongodb server ***"
-set -o errexit
-
-/docker-base-init/docker-boot
-
-/docker-base-init/set-PS1 MongoDB
-
-su - u01 -c "mkdir -p /data/mongodb; mongod --dbpath /data/mongodb"
--- develop-tools/react-native/files/start-nodejs-shell-rn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-/docker-react-native-init/docker-react-native-boot
-
-su - u01
