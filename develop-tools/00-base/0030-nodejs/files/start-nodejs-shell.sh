f98830874f88948e9ea646714ddb6d39a073da5c
thinkbase <thinkbase.net@gmail.com>
2021-07-03 18:11:24 +0800

    split docker-*-init and start-*.sh - docker-*-init should only include statements which be inherited by it's extended images

--- develop-tools/00-base/0030-nodejs/files/start-nodejs-shell.sh
+++ develop-tools/00-base/0030-nodejs/files/start-nodejs-shell.sh
@@ -3,4 +3,10 @@ set -o errexit
 
 /docker-nodejs-init/docker-node-boot
 
+# To mark the environment variables which should be used in "u01"'s shell
+/docker-base-init/set-fixed-ENVs http_proxy https_proxy
+
+/docker-base-init/set-PS1 NodeJS
+/docker-base-init/set-CMD "cd /workspace"
+
 su - u01
--- develop-tools/00-base/0050-code-server/files/start-cdr.sh
+++ develop-tools/00-base/0050-code-server/files/start-cdr.sh
@@ -2,3 +2,17 @@
 set -o errexit
 
 /docker-cdr-init/docker-cdr-boot
+
+# To mark the environment variables which should be used in "u01"'s shell
+/docker-base-init/set-fixed-ENVs http_proxy https_proxy
+
+# code-server environment
+/docker-base-init/set-PS1 code-server
+/docker-base-init/set-CMD "cd /workspace"
+
+echo "*** Starting code-server ***"
+su - u01 -c ' \
+/usr/bin/code-server \
+    --auth none \
+    --bind-addr 0.0.0.0:8080
+'
