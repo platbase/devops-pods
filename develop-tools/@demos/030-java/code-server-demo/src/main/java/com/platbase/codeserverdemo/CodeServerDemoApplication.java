f3f0f8473efac56d94dbf1d971d3ad25a784396b
thinkbase <thinkbase.net@gmail.com>
2021-08-05 22:09:04 +0800

    feature: Add sample java project for code-server development environment for java(jdk 8)

---
+++ develop-tools/@demos/030-java/code-server-demo/src/main/java/com/platbase/codeserverdemo/CodeServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.platbase.codeserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeServerDemoApplication.class, args);
+	}
+
+}
---
+++ develop-tools/@demos/030-java/code-server-demo/src/test/java/com/platbase/codeserverdemo/CodeServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platbase.codeserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
