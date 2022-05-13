c7d2abf951579b41837ea0a9edc95175df7175c0
thinkbase <thinkbase.net@gmail.com>
2022-05-14 03:46:26 +0800

    SSH tunnel server and client, test version

---
+++ servers/90-apps/9021-sshd-t-client/resources/docker-init/etc/root.d/00-prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+# Remove u01 from sudo
+sudo -l -U u01
+gpasswd -d u01 sudo
+sudo -l -U u01
+
+set +o errexit
+# Detect the host IP and bind it to hostname "docker-host"
+echo "... Detect IP of docker host and bind it's hostname to 'docker-host'"
+cat /etc/hosts | grep "docker-host"
+if [ $? > 0 ]; then
+    echo "# Add docker host's IP address(The same as the default gateway)"    >> /etc/hosts
+    echo "$(/sbin/ip route|awk '/default/ { print $3 }')     docker-host"     >> /etc/hosts
+fi
+
+sleep 3000
