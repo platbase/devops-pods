2ea58f9464f43bb1e1209dabd515378fcf86d295
cloud-ide <cloud-ide@cloud-ide>
2022-06-10 23:16:07 +0800

    upgrade to code-server 4.4.0

--- devenv/00-base/0050-code-server/commands/add-extensions.sh
+++ devenv/00-base/0050-code-server/commands/add-extensions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -o errexit
 
+mkdir -p ${VSC_EXT_CACHE}
 for ext in "$@"; do
-    code-server --extensions-dir /usr/local/share/code-server/extensions --install-extension $ext
+    code-server --extensions-dir ${VSC_EXT_CACHE} --install-extension $ext
 done
 
-chown -Rv u01:u01 /usr/local/share/code-server/extensions | (sed -u 7q; echo "..."; tail -n 7)
+chown -Rv u01:u01 ${VSC_EXT_CACHE} | (sed -u 7q; echo "..."; tail -n 7)
--- devenv/00-base/0050-code-server/files/docker-cdr-init/docker-cdr-install-extensions.sh
+++ devenv/00-base/0050-code-server/files/docker-cdr-init/docker-cdr-install-extensions.sh
@@ -2,11 +2,11 @@
 set -o errexit
 
 # move to local: pre-installed extensions
-if [ -d "/usr/local/share/code-server/extensions/" ]; then
-	for file in $(ls /usr/local/share/code-server/extensions/); do
+if [ -d "${VSC_EXT_CACHE}/" ]; then
+	for file in $(ls ${VSC_EXT_CACHE}/); do
 	    if [ ! -d "/data/home/.local/share/code-server/extensions/$file" ]; then
 	    	echo "... Prepare extension :'/data/home/.local/share/code-server/extensions/$file' ..."
-	        mv "/usr/local/share/code-server/extensions/$file" "/data/home/.local/share/code-server/extensions/$file"
+	        mv "${VSC_EXT_CACHE}/$file" "/data/home/.local/share/code-server/extensions/$file"
 	    fi
 	done
 fi
