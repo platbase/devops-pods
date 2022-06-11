bee6aa9b028b5b8b02637c3414a25280060fb561
thinkbase <thinkbase.net@gmail.com>
2022-06-11 11:58:12 +0800

    Fix: Avoid invalid permission of private keys: @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ @         WARNING: UNPROTECTED PRIVATE KEY FILE!          @ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@

--- servers/90-apps/9020-ssh-tunnel/sshd-t-server/resources/docker-init/etc/root.d/00-prepare.sh
+++ servers/90-apps/9020-ssh-tunnel/sshd-t-server/resources/docker-init/etc/root.d/00-prepare.sh
@@ -13,6 +13,11 @@ echo "u01:${U01_SSH_PWD}" | chpasswd
 # Change SSH Port
 echo "Port ${SSH_PORT}" >> /etc/ssh/sshd_config
 
+# Overwrite permission of private keys
+ls -al /etc/ssh/ssh_host_*_key
+chmod -v 600 /etc/ssh/ssh_host_*_key
+ls -al /etc/ssh/ssh_host_*_key
+
 # Start SSH Server
 mkdir -p /var/run/sshd
 /usr/sbin/sshd -D -e
